2c3bc08cff27e18dcc19f115a7bd92ad91f105fb
Harry Markley <tremarkley@gmail.com>
2018-03-19 16:31:03 -0700

    Update postgres controller to handle restaurant_id

--- db_pg/controller.js
+++ db_pg/controller.js
@@ -2,7 +2,7 @@ const db = require('./db');
 
 const findOneById = async (id) => {
   try {
-    return db.one('SELECT * FROM restaurants where _id = $1', [id]);
+    return db.one('SELECT * FROM restaurants where restaurant_id = $1', [id]);
   } catch (error) {
     throw new Error(error);
   }
