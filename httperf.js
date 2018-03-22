4366015ebb17b4bac50deab121267250fb6d6899
Harry Markley <tremarkley@gmail.com>
2018-03-21 19:28:36 -0700

    Setup queryTimer for mongo, refactored httperf.js to be multithreaded

--- httperf.js
+++ httperf.js
@@ -1,13 +1,36 @@
 const util = require('util');
 const exec = util.promisify(require('child_process').exec);
+const cluster = require('cluster');
+const numCPUs = require('os').cpus().length;
 const generateUrls = require('./generateUrls');
 
-generateUrls(1000000);
-
 async function ls() {
-  const { stdout, stderr } = await exec('httperf --server localhost --port 3002 --wlog Y,wlog.log --num-conns 10 --rate 10 --timeout 1');
-  console.log('stdout:', stdout);
-  console.log('stderr:', stderr);
+  try {
+    const { stdout, stderr } = await exec('httperf --server localhost --port 3002 --wlog Y,wlog.log --num-conns 100 --rate 100 --timeout 1');
+    console.log('stdout:', stdout);
+    console.log('stderr:', stderr);
+  } catch (error) {
+    console.log(`error executing command: ${error}`);
+  } finally {
+    process.exit();
+  }
 }
-ls();
+
+if (cluster.isMaster) {
+  generateUrls(1000000);
+
+  for (let i = 0; i < numCPUs; i += 1) {
+    cluster.fork();
+  }
+  let countExits = 0;
+  cluster.on('exit', () => {
+    console.log('process exitted');
+    countExits += 1;
+    if (countExits === numCPUs) {
+      process.exit();
+    }
+  });
+} else {
+  ls();
+}
 
