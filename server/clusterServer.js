edd50ea9b12e08acf52cac63d47a7a7ceec6215b
Harry Markley <tremarkley@gmail.com>
2018-03-22 15:35:26 -0700

    Add new relic to multithreaded server

--- server/clusterServer.js
+++ server/clusterServer.js
@@ -1,5 +1,6 @@
 const cluster = require('cluster');
 const numCPUs = require('os').cpus().length;
+require('newrelic');
 
 const { connectToServer } = require('../db_mongo/db');
 const app = require('./app');
@@ -15,15 +16,19 @@ const masterProcess = function masterProcess() {
 
 const childProcess = function childProcess() {
   console.log(`Worker ${process.pid} started...`);
-  connectToServer()
-    .then(() => {
-      app.listen(3002, () => {
-        console.log('Listening on port 3002');
-      });
-    })
-    .catch((err) => {
-      console.log(`unable to connect to mongo: ${err}`);
-    });
+  // connectToServer()
+  //   .then(() => {
+  //     app.listen(3002, () => {
+  //       console.log('Listening on port 3002');
+  //     });
+  //   })
+  //   .catch((err) => {
+  //     console.log(`unable to connect to mongo: ${err}`);
+  //   });
+
+  app.listen(3002, () => {
+    console.log('Listening on port 3002');
+  });
 };
 
 if (cluster.isMaster) {
