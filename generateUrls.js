ba9afcc402f16a275b520bb0b6f2384e306101ce
Harry Markley <tremarkley@gmail.com>
2018-03-22 14:55:31 -0700

    Setup configuration for artillery load testing

--- generateUrls.js
+++ generateUrls.js
@@ -1,27 +1,44 @@
 //  when running httperf
 //  httperf --server localhost --port 3002
 //    --wlog Y,wlog.log --num-conns 10000 --rate 100 --timeout 1
-const fs = require('fs');
+// const fs = require('fs');
 
-const generateUrls = function generateUrls(n) {
-  let count = n;
-  const size = 1000000;
-  while (count > 0) {
-    const urls = [''];
-    for (let i = 0; i < size; i += 1) {
-      const id = Math.floor(Math.random() * 10000000);
-      urls.push(`/restaurants/${id}/`);
-      urls.push(`/restaurants/${id}/bundle.js`);
-      urls.push(`/api/restaurants/${id}/overview`);
-    }
-    if (count === n) {
-      fs.writeFileSync('wlog.log', urls.join('\0'));
-    } else {
-      fs.appendFileSync('wlog.log', urls.join('\0'));
-    }
-    count -= size;
-  }
+// const generateUrls = function generateUrls(n) {
+//   let count = n;
+//   const size = 1000000;
+//   while (count > 0) {
+//     const urls = [''];
+//     for (let i = 0; i < size; i += 1) {
+//       const id = Math.floor(Math.random() * 10000000);
+//       urls.push(`/restaurants/${id}/`);
+//       urls.push(`/restaurants/${id}/bundle.js`);
+//       urls.push(`/api/restaurants/${id}/overview`);
+//     }
+//     if (count === n) {
+//       fs.writeFileSync('wlog.log', urls.join('\0'));
+//     } else {
+//       fs.appendFileSync('wlog.log', urls.join('\0'));
+//     }
+//     count -= size;
+//   }
+// };
+
+// const generateUrls = function generateUrls(n = 100) {
+const generateUrls = function generateUrls(userContext, events, done) {
+  // const urls = [''];
+  // // const ids = [];
+  // for (let i = 0; i < n; i += 1) {
+  //   const id = Math.floor(Math.random() * 10000000);
+  // ids.push(id);
+  // urls.push(`/restaurants/${id}/`);
+  // urls.push(`/restaurants/${id}/bundle.js`);
+  // urls.push(`/api/restaurants/${id}/overview`);
+  const id = Math.floor(Math.random() * 10000001);
+  userContext.vars.restaurantId = id;
+  return done();
 };
 
-module.exports = generateUrls;
+module.exports = {
+  generateUrls,
+};
 
