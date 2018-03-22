ffde62c3361a7249ca0ba32155b2246f8d998619
Harry Markley <tremarkley@gmail.com>
2018-03-22 15:31:51 -0700

    Modify generateUrls to return less random data

--- generateUrls.js
+++ generateUrls.js
@@ -25,15 +25,19 @@
 
 // const generateUrls = function generateUrls(n = 100) {
 const generateUrls = function generateUrls(userContext, events, done) {
-  // const urls = [''];
-  // // const ids = [];
-  // for (let i = 0; i < n; i += 1) {
-  //   const id = Math.floor(Math.random() * 10000000);
-  // ids.push(id);
-  // urls.push(`/restaurants/${id}/`);
-  // urls.push(`/restaurants/${id}/bundle.js`);
-  // urls.push(`/api/restaurants/${id}/overview`);
-  const id = Math.floor(Math.random() * 10000001);
+  // keep the majority of requests in a small range
+  const randomNumber = Math.random();
+  let id;
+  if (randomNumber < 0.85) {
+    // 85% of the time the requests will hit id's 1,000 through 1050
+    id = Math.floor(Math.random() * 50) + 1000;
+  } else if (randomNumber >= 0.85 && randomNumber < 0.95) {
+    // 10% of requests will fall over 50,000 through 50,500
+    id = Math.floor(Math.random() * 500) + 50000;
+  } else {
+    //  rest of requests will be completely random
+    id = Math.floor(Math.random() * 10000001);
+  }
   userContext.vars.restaurantId = id;
   return done();
 };
