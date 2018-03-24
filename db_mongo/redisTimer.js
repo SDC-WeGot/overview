5ada181e584ceb682b5b496679c05047ce6bc630
Harry Markley <tremarkley@gmail.com>
2018-03-24 16:36:11 -0700

    Configure redis cluster

--- db_mongo/redisTimer.js
+++ db_mongo/redisTimer.js
@@ -1,9 +1,30 @@
-const redis = require('redis');
+// const redis = require('redis');
+const Redis = require('ioredis');
 const { generateUrls } = require('../generateUrls');
 const Promise = require('bluebird');
 
-const REDIS_PORT = 6379;
-const client = Promise.promisifyAll(redis.createClient({ host: 'localhost', port: REDIS_PORT }));
+// const REDIS_PORT = 6379;
+// const client = Promise.promisifyAll(redis.createClient({ host: 'localhost', port: REDIS_PORT }));
+
+const cluster = Promise.promisifyAll(new Redis.Cluster([{
+  port: 30001,
+  host: '127.0.0.1',
+}, {
+  port: 30002,
+  host: '127.0.0.1',
+}, {
+  port: 30003,
+  host: '127.0.0.1',
+}, {
+  port: 30004,
+  host: '127.0.0.1',
+}, {
+  port: 30005,
+  host: '127.0.0.1',
+}, {
+  port: 30006,
+  host: '127.0.0.1',
+}]));
 
 const queryAsync = async function queryAsync(n) {
   const obj = { };
@@ -17,7 +38,7 @@ const queryAsync = async function queryAsync(n) {
     generateUrls(obj, null, done);
     const startTime = new Date().getTime();
     try {
-      await client.getAsync(obj.vars.restaurantId);
+      await cluster.getAsync(obj.vars.restaurantId);
       times.push(new Date().getTime() - startTime);
     } catch (error) {
       console.log(`error retrieving id: ${error}`);
--- server/cache.js
+++ server/cache.js
@@ -1,11 +1,32 @@
-const redis = require('redis');
+// const redis = require('redis');
+const Redis = require('ioredis');
 
-const REDIS_PORT = 6379;
-const client = redis.createClient({ host: 'localhost', port: REDIS_PORT });
+const cluster = new Redis.Cluster([{
+  port: 30001,
+  host: '127.0.0.1',
+}, {
+  port: 30002,
+  host: '127.0.0.1',
+}, {
+  port: 30003,
+  host: '127.0.0.1',
+}, {
+  port: 30004,
+  host: '127.0.0.1',
+}, {
+  port: 30005,
+  host: '127.0.0.1',
+}, {
+  port: 30006,
+  host: '127.0.0.1',
+}]);
+
+// const REDIS_PORT = 6379;
+// const client = redis.createClient({ host: 'localhost', port: REDIS_PORT });
 
 const cache = function cache(req, res, next) {
   const { id } = req.params;
-  client.get(id, (err, data) => {
+  cluster.get(id, (err, data) => {
     if (err) { throw err; }
     if (data !== null) {
       res.send(data);
