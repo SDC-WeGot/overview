a869cc749b2622a99c458182ccfb197e626d5897
Harry Markley <tremarkley@gmail.com>
2018-03-26 16:19:45 -0700

    Modify webpack config to handle server side rendering

--- webpack.config.js
+++ webpack.config.js
@@ -4,19 +4,13 @@ const webpack = require('webpack');
 const SRC_DIR = path.join(__dirname, '/client/src');
 const DIST_DIR = path.join(__dirname, '/client/dist');
 
-module.exports = {
+const common = {
   resolve: {
     extensions: ['.js', '.jsx']
   },
-  devtool: 'source-map',
-  entry: `${SRC_DIR}/index.jsx`,
-  output: {
-    path: DIST_DIR,
-    publicPath: '/',
-    filename: 'bundle.js'
-  },
-  module : {
-    loaders : [
+  context: SRC_DIR,
+  module: {
+    loaders: [
       {
         test: /\.jsx?/,
         include: SRC_DIR,
@@ -50,3 +44,73 @@ module.exports = {
     })
   ],
 };
+
+const client = {
+  entry: SRC_DIR + '/client.js',
+  output: {
+    path: DIST_DIR,
+    filename: 'app.js'
+  }
+};
+
+const server = {
+  entry: SRC_DIR + '/server.js',
+  target: 'node',
+  output: {
+    path: DIST_DIR,
+    filename: 'app-server.js',
+    libraryTarget: 'commonjs-module'
+  }
+};
+
+module.exports = [
+  Object.assign({}, common, client),
+  Object.assign({}, common, server)
+];
+
+// module.exports = {
+//   resolve: {
+//     extensions: ['.js', '.jsx']
+//   },
+//   devtool: 'source-map',
+//   entry: `${SRC_DIR}/index.jsx`,
+//   output: {
+//     path: DIST_DIR,
+//     publicPath: '/',
+//     filename: 'bundle.js'
+//   },
+//   module : {
+//     loaders : [
+//       {
+//         test: /\.jsx?/,
+//         include: SRC_DIR,
+//         exclude: ['node_modules'],
+//         loader: 'babel-loader',      
+//         query: {
+//           presets: ['react', 'env']
+//         }
+//       },
+//       {
+//         test: /\.css$/,
+//         use: [
+//           'style-loader',
+//           'css-loader'
+//         ]
+//       }
+//     ]
+//   },
+//   plugins: [
+//     new webpack.DefinePlugin({
+//       'process.env': {
+//         'NODE_ENV': JSON.stringify('production')
+//       }
+//     }),
+//     new CompressionPlugin({  
+//       asset: "[path].gz[query]",
+//       algorithm: "gzip",
+//       test: /\.js$|\.css$|\.html$/,
+//       threshold: 10240,
+//       minRatio: 0.8
+//     })
+//   ],
+// };
