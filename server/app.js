098a82748bf958fcb7e9e04ce8e2bc7d095e9626
Harry Markley <tremarkley@gmail.com>
2018-03-28 15:51:39 -0700

    Configure app to return bundle.js

--- server/app.js
+++ server/app.js
@@ -1,5 +1,5 @@
 const express = require('express');
-//  const morgan = require('morgan');
+ const morgan = require('morgan');
 const cors = require('cors');
 const path = require('path');
 const handler = require('./routes/requestHandler.js');
@@ -9,7 +9,7 @@ const cache = require('./cache');
 const app = express();
 
 app.use(cors());
-// app.use(morgan('dev'));
+app.use(morgan('dev'));
 
 app.get('/', (req, res) => {
   //  res.redirect('/restaurants/ChIJUcXYWWGAhYARmjMY2bJAG2s');
@@ -22,6 +22,16 @@ app.get('*bundle.js', (req, res) => {
   res.set('Content-Encoding', 'gzip');
   res.download(path.resolve(__dirname, '../client/dist/bundle.js.gz'));
 });
+
+app.get('/app.js', (req, res) => {
+  res.download(path.resolve(__dirname, '../client/dist/app.js'));
+});
+
+app.get('/app-server.js', (req, res) => {
+  res.download(path.resolve(__dirname, '../client/dist/app-server.js'));
+});
+
+// app.use(express.static('client/dist'));
 // app.use('/restaurants/:id', express.static('client/dist'));
 //  TODO implement this route so that proxy server can download bundle.js
 // app.get('/download', (req, res) => {
