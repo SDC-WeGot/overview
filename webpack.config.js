3e9dccb570f902ce9e77d3a099fc0a42e36d8b9e
Harry Markley <tremarkley@gmail.com>
2018-03-29 15:48:36 -0700

    Clean up comments in webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -9,26 +9,6 @@ const common = {
     extensions: ['.js', '.jsx']
   },
   context: SRC_DIR,
-  // module: {
-  //   loaders: [
-  //     // {
-  //     //   test: /\.jsx?/,
-  //     //   include: SRC_DIR,
-  //     //   exclude: ['node_modules'],
-  //     //   loader: 'babel-loader',      
-  //     //   query: {
-  //     //     presets: ['react', 'env']
-  //     //   }
-  //     // },
-  //     // {
-  //     //   test: /\.css$/,
-  //     //   use: [
-  //     //     'style-loader',
-  //     //     'css-loader'
-  //     //   ]
-  //     // }
-  //   ]
-  //  },
   plugins: [
     new webpack.DefinePlugin({
       'process.env': {
@@ -104,50 +84,3 @@ module.exports = [
   Object.assign({}, common, client),
   Object.assign({}, common, server)
 ];
-
-// module.exports = {
-//   resolve: {
-//     extensions: ['.js', '.jsx']
-//   },
-//   devtool: 'source-map',
-//   entry: `${SRC_DIR}/index.jsx`,
-//   output: {
-//     path: DIST_DIR,
-//     publicPath: '/',
-//     filename: 'bundle.js'
-//   },
-//   module : {
-//     loaders : [
-//       {
-//         test: /\.jsx?/,
-//         include: SRC_DIR,
-//         exclude: ['node_modules'],
-//         loader: 'babel-loader',      
-//         query: {
-//           presets: ['react', 'env']
-//         }
-//       },
-//       {
-//         test: /\.css$/,
-//         use: [
-//           'style-loader',
-//           'css-loader'
-//         ]
-//       }
-//     ]
-//   },
-//   plugins: [
-//     new webpack.DefinePlugin({
-//       'process.env': {
-//         'NODE_ENV': JSON.stringify('production')
-//       }
-//     }),
-//     new CompressionPlugin({  
-//       asset: "[path].gz[query]",
-//       algorithm: "gzip",
-//       test: /\.js$|\.css$|\.html$/,
-//       threshold: 10240,
-//       minRatio: 0.8
-//     })
-//   ],
-// };
