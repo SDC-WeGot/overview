12d65fc8bcbd0a94b855256127a6843f694eea64
Harry Markley <tremarkley@gmail.com>
2018-03-29 15:37:25 -0700

    Add server side rendering style to weGotDividerLine

--- client/src/components/WeGotDividerLine.jsx
+++ client/src/components/WeGotDividerLine.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
+import styles from '../style.css';
 
 const DividerLine = () => (
-  <div id="overview-divider-line">
-    <hr id="overview-hline" />
-    <img id="overview-logo" src="http://res.cloudinary.com/madlicorice/image/upload/c_scale,q_100,w_46/v1520407902/WeGot_small_logo_recolored.png" alt="wegot-logo" />
+  <div id="overview-divider-line" className={styles.overviewDividerLine}>
+    <hr id="overview-hline" className={styles.overviewHline} />
+    <img id="overview-logo" className={styles.overviewLogo} src="http://res.cloudinary.com/madlicorice/image/upload/c_scale,q_100,w_46/v1520407902/WeGot_small_logo_recolored.png" alt="wegot-logo" />
   </div>
 );
 
