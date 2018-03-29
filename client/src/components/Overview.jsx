8eb7a359f1fbfbfbfde36329d7f319608fa5c9b7
Harry Markley <tremarkley@gmail.com>
2018-03-28 19:28:42 -0700

    Configure all props to be passed into Overview component so api calls are made outside of component

--- client/src/components/Overview.jsx
+++ client/src/components/Overview.jsx
@@ -8,85 +8,118 @@ import LongDescription from './LongDescription';
 
 class Overview extends React.Component {
   constructor(props) {
+    console.log(`props: ${JSON.stringify(props)}`);
     super(props);
-    this.state = {
-      renderBool: false,
-      restaurantTitle: 'Title Placeholder',
-      restaurantTagline: 'Tagline Placeholder',
-      restaurantType: 'Restaurant',
-      restaurantVicinity: 'Vicinity Placeholder',
-      restaurantPriceLevel: 'Price Level Placeholder',
-      weGotFoodRating: '3.3',
-      weGotDecorRating: '3.3',
-      weGotServiceRating: '3.3',
-      restaurantDescription: 'Description Placeholder',
-    };
-  }
-
-  componentDidMount() {
-    this.fetchRestaurantInfo();
-  }
-
-  fetchRestaurantInfo() {
-    // const id = window.location.href.split('/')[4];
-    axios.get(`http://localhost:3002/api/restaurants/${this.props.itemid}/overview`)
-      .then((response) => {
-        this.handleRestaurantChange(response.data);
-      })
-      .catch((err) => {
-        console.log(err);
-      });
-  }
-
-  handleRestaurantChange(restaurantDetails) {
+    // this.state = {
+    //   renderBool: false,
+    //   restaurantTitle: 'Title Placeholder',
+    //   restaurantTagline: 'Tagline Placeholder',
+    //   restaurantType: 'Restaurant',
+    //   restaurantVicinity: 'Vicinity Placeholder',
+    //   restaurantPriceLevel: 'Price Level Placeholder',
+    //   weGotFoodRating: '3.3',
+    //   weGotDecorRating: '3.3',
+    //   weGotServiceRating: '3.3',
+    //   restaurantDescription: 'Description Placeholder',
+    // };
     let priceLevelInDollars = '';
-    const priceLevel = restaurantDetails.priceLevel || 1;
+    const priceLevel = this.props.priceLevel || 1;
     for (let i = 0; i < priceLevel; i += 1) {
       priceLevelInDollars += '$';
     }
-    this.setState({
-      renderBool: true,
-      restaurantTitle: restaurantDetails.name.toUpperCase(),
-      restaurantTagline: restaurantDetails.tagline,
-      restaurantVicinity: restaurantDetails.vicinity,
-      restaurantPriceLevel: priceLevelInDollars,
-      weGotFoodRating: restaurantDetails.zagatFood,
-      weGotDecorRating: restaurantDetails.zagatDecor,
-      weGotServiceRating: restaurantDetails.zagatService,
-      restaurantDescription: restaurantDetails.longDescription,
-    });
+    this.props.priceLevel = priceLevelInDollars;
   }
 
+  // componentDidMount() {
+  //   this.fetchRestaurantInfo();
+  // }
+
+  // componentWillMount() {
+  //   console.log('component will mount');
+  //   (async () => {
+  //     try {
+  //       const response = await axios.get(`http://localhost:3002/api/restaurants/${this.props.itemid}/overview`);
+  //       await this.handleRestaurantChange(response.data);
+  //       console.log(`state prior to mount: ${JSON.stringify(this.state)}`);
+  //     } catch (e) {
+  //       //  ...handle the error...
+  //       console.log(`error in componentWillMount: ${e}`);
+  //     }
+  //   })();
+  // }
+
+  // fetchRestaurantInfo() {
+  //   // const id = window.location.href.split('/')[4];
+  //   axios.get(`http://localhost:3002/api/restaurants/${this.props.itemid}/overview`)
+  //     .then((response) => {
+  //       this.handleRestaurantChange(response.data);
+  //     })
+  //     .catch((err) => {
+  //       console.log(err);
+  //     });
+  // }
+
+  // handleRestaurantChange(restaurantDetails) {
+  //   let priceLevelInDollars = '';
+  //   const priceLevel = restaurantDetails.priceLevel || 1;
+  //   for (let i = 0; i < priceLevel; i += 1) {
+  //     priceLevelInDollars += '$';
+  //   }
+  //   return new Promise((resolve) => {
+  //     this.setState({
+  //       renderBool: true,
+  //       restaurantTitle: restaurantDetails.name.toUpperCase(),
+  //       restaurantTagline: restaurantDetails.tagline,
+  //       restaurantVicinity: restaurantDetails.vicinity,
+  //       restaurantPriceLevel: priceLevelInDollars,
+  //       weGotFoodRating: restaurantDetails.zagatFood,
+  //       weGotDecorRating: restaurantDetails.zagatDecor,
+  //       weGotServiceRating: restaurantDetails.zagatService,
+  //       restaurantDescription: restaurantDetails.longDescription,
+  //     }, resolve);
+  //   });
+  // }
+
   render() {
-    if (this.state.renderBool) {
-      return (
-        <div id="overview-wrapper">
-          <div id="overview-restaurant-title">{this.state.restaurantTitle}</div>
-          <div id="overview-restaurant-tagline">{this.state.restaurantTagline}</div>
-          <BasicDetails
-            type={this.state.restaurantType}
-            vicinity={this.state.restaurantVicinity}
-            priceLevel={this.state.restaurantPriceLevel}
-          />
-          <DividerLine />
-          <div className="overview-wegot-review-title">THE WEGOT REVIEW</div>
-          <WeGotReview
-            food={this.state.weGotFoodRating}
-            decor={this.state.weGotDecorRating}
-            service={this.state.weGotServiceRating}
-          />
-          <LongDescription
-            description={this.state.restaurantDescription}
-          />
-        </div>
-      );
-    }
-    return <div>Loading Restaurant Info...</div>;
+    // if (this.state.renderBool) {
+    return (
+      <div id="overview-wrapper">
+        <div id="overview-restaurant-title">{this.props.name.toUpperCase()}</div>
+        <div id="overview-restaurant-tagline">{this.props.tagline}</div>
+        <BasicDetails
+          type={this.props.type}
+          vicinity={this.props.vicinity}
+          priceLevel={this.props.priceLevel}
+        />
+        <DividerLine />
+        <div className="overview-wegot-review-title">THE WEGOT REVIEW</div>
+        <WeGotReview
+          food={this.props.zagatFood}
+          decor={this.props.zagatDecor}
+          service={this.props.zagatService}
+        />
+        <LongDescription
+          description={this.props.longDescription}
+        />
+      </div>
+    );
   }
+  //   return <div>Loading Restaurant Info...</div>;
+  // }
 }
 
 Overview.propTypes = {
-  itemid: PropTypes.number.isRequired,
+  _id: PropTypes.string.isRequired,
+  restaurant_id: PropTypes.number.isRequired,
+  name: PropTypes.string.isRequired,
+  tagline: PropTypes.string.isRequired,
+  type: PropTypes.string.isRequired,
+  vicinity: PropTypes.string.isRequired,
+  priceLevel: PropTypes.string.isRequired,
+  zagatFood: PropTypes.string.isRequired,
+  zagatDecor: PropTypes.string.isRequired,
+  zagatService: PropTypes.string.isRequired,
+  longDescription: PropTypes.string.isRequired,
 };
 
 export default Overview;
