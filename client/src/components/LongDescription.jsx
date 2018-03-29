a9ee5b120dd67e23694c47cdc9e2d10bd75ee227
Harry Markley <tremarkley@gmail.com>
2018-03-29 15:31:44 -0700

    Added styles for LongDescription Component

--- client/src/components/LongDescription.jsx
+++ client/src/components/LongDescription.jsx
@@ -1,8 +1,9 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import styles from '../style.css';
 
 const LongDescription = (({ description }) => (
-  <div id="overview-long-description">
+  <div id="overview-long-description" className={styles.overviewLongDescription}>
     {description}
   </div>
 ));
